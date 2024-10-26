59e21a19c0c509636beb8d2e3595553216da3802
d-minina <87333725+DariaMinina@users.noreply.github.com>
2024-10-26 12:18:53 +0300

    fixed path to run.py file

--- server/run.py
+++ server/run.py
@@ -1,5 +1,12 @@
+import sys
+import os
+
+# Добавляем корневую директорию проекта в PATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from server.app.create_app import create_app
 from server.app.config import Config
+
 
 app, db = create_app()
 

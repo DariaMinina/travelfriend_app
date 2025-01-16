5081b214b986fbf347d5caa33e4c7437a4e8e81d
dashok_770 <87333725+DariaMinina@users.noreply.github.com>
2025-01-16 19:49:35 +0300

    Update create_friendship.sql

--- server/app/sql/create_friendship.sql
+++ server/app/sql/create_friendship.sql
@@ -4,6 +4,5 @@ CREATE TABLE app.friendship (
 	CONSTRAINT friendship_pk PRIMARY KEY (user_id, friend_id)
 );
 
-
 ALTER TABLE app.friendship ADD CONSTRAINT friendship_users_fk FOREIGN KEY (user_id) REFERENCES app.users(id) ON UPDATE CASCADE;
 ALTER TABLE app.friendship ADD CONSTRAINT friendship_users_fk_1 FOREIGN KEY (friend_id) REFERENCES app.users(id) ON UPDATE CASCADE;
